26c575fbd83369ed52bdd8f108925356448cfc48
maciej.kulon <maciej.kulon@dazn.com>
2023-01-13 13:16:22 +0100

    feat(): change ffprobe to run in parallel

--- src/segments-handler/segments-handler.controller.ts
+++ src/segments-handler/segments-handler.controller.ts
@@ -2,7 +2,7 @@ import { Controller } from '@nestjs/common';
 import { types } from 'hls-parser';
 import { RMQRoute, RMQService, RMQTransform } from 'nestjs-rmq';
 import { FFprobeService } from 'src/ffprobe/ffprobe-service';
-import { MediaPlaylistResult } from 'src/types/types';
+import { MediaPlaylistResult, SegmentResult } from 'src/types/types';
 import { SegmentsHandlerService } from './segments-handler.service';
 
 @Controller()
@@ -15,22 +15,36 @@ export class SegmentsHandlerController {
   @RMQTransform()
   @RMQRoute('media.playlist.created')
   public async handleSegments(mediaPlaylist: types.MediaPlaylist) {
-    const mediaPlaylistResult: MediaPlaylistResult = {
-      segments: [],
-      uri: mediaPlaylist.uri,
-    };
-
     const segmentsUrls = await this.segmentsHandler.createSegmentsUrls(
       mediaPlaylist,
     );
 
+    const promises: Promise<SegmentResult>[] = [];
     for (const segmentUrl of segmentsUrls) {
-      const ffprobeData = await this.ffprobeService.getFprobeData(segmentUrl);
-      mediaPlaylistResult.segments.push({
-        ffprobeData,
-        uri: segmentUrl,
-      });
+      promises.push(
+        new Promise((resolve, reject) => {
+          this.ffprobeService
+            .getFprobeData(segmentUrl)
+            .then((ffprobeData) => {
+              resolve({
+                ffprobeData,
+                uri: segmentUrl,
+              });
+            })
+            .catch((err) => {
+              console.error(err);
+              reject(err);
+            });
+        }),
+      );
     }
+
+    const segmentsResults = await Promise.all(promises);
+
+    const mediaPlaylistResult: MediaPlaylistResult = {
+      segments: segmentsResults,
+      uri: mediaPlaylist.uri,
+    };
 
     console.log(mediaPlaylistResult);
 
